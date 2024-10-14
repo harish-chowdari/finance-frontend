2fea2669b9f0edc5c2de2248faa5963014b7187f
harish-nagubadi <Harish.N@veots.com>
2024-10-14 11:43:02 +0530

    first commit10

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "https://finance-backend-g1be.onrender.com" 
+const BASE_URL = "https://finance-backend-g1be.onrender.com/api" 
 
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
